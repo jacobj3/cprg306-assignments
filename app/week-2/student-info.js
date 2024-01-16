352f129f26ee32a92e6b22cfcb51c9b7160a2c63
jacobj3 <131091976+jacobj3@users.noreply.github.com>
2024-01-16 13:57:43 -0700

    Add student-info.js code.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <p>Jacob Jingco</p>
+      <p>
+        <Link href={"https://github.com/jacobj3/cprg306-assignments"}>
+          https://github.com/jacobj3/cprg306-assignments
+        </Link>
+      </p>
+    </main>
+  );
+}
