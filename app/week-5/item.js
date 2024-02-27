f36d54104a514f7bd5e7ef6061cd77ae0d52c6a8
jacobj3 <131091976+jacobj3@users.noreply.github.com>
2024-02-27 14:00:00 -0700

    Update item.js

--- app/week-5/item.js
+++ app/week-5/item.js
@@ -1,10 +1,14 @@
 export default function Item(props) {
   return (
-    <li className="px-2 bg-gradient-to-r from-blue-900 to-gray-800 m-5 max-w-xs rounded-md">
-      <h2 className="font-bold">{props.name}</h2>
-      <p className="mb-4">
-        Buy {props.quantity} in {props.category}
-      </p>
-    </li>
+    <ul className="px-2 bg-gradient-to-r from-blue-900 to-gray-800 m-5 max-w-xs rounded-md">
+      <li>
+        <h2 className="font-bold">{props.name}</h2>
+      </li>
+      <li>
+        <p className="mb-4">
+          Buy {props.quantity} in {props.category}
+        </p>
+      </li>
+    </ul>
   );
 }
